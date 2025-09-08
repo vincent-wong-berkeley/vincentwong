7d17060d67ea2ca005e1de9daa5985f9f7a773c8
Vincent <vincentwong@berkeley.edu>
2025-09-08 16:38:07 -0700

    Saving homework1/ folder

---
+++ datatypes.py
@@ -0,0 +1,22 @@
+print("Hello, World")
+
+"""
+#This is a comment
+# """
+
+x = "Hello, World"
+
+#print(x)
+#print(type(x))
+
+#Integers
+y = 5
+print(y)
+print(type(y))
+
+#Floats
+z = 2.5
+print(z)
+print(type(z))
+
+print(type(7.1))
---
+++ homework1/homework1.py
@@ -0,0 +1,67 @@
+# --- Variables and Data Types --- 
+
+a = 10 
+print(a)
+print(type(a))
+# a is an interger. whole numbers
+
+b = 1.5
+print (b)
+print(type(b))
+# b is a float. fractional number or number with decimals
+
+c = 3j
+print (c)
+print(type(c))
+# c is a string. words or text
+
+d = "hello"
+print (d)
+print(type(d))
+# d is a string. words or text
+
+e = [1, 2, 3]
+print (e)
+print(type(e))
+# e is a list.
+
+f = {"name": "Ellen", "favorite fruit": "strawberry"}
+print (f)
+print(type(f))
+# f is a dictionary. Stores information in unique key-value pairs
+
+g = (1, 2)
+print (g)
+print (type(g))
+# g is a tuple. assigns a tuple containing the integer values (1, 2) to the variable (g)
+
+h = ["apple", "banana", "strawberry"]
+print (h)
+print (type(h))
+# h is a list.
+
+i = True
+print (i)
+print (type(i))
+# i is a bool.
+
+j = None
+print (j)
+print (type(j))
+# j is a NonType.
+
+k = [True, "blue", 12]
+print(k)
+print(type (k))
+# k is a list.
+
+l = str(14)
+print(l)
+print(type (l))
+# k is a string
+
+m = 1e4
+print(m)
+print(type (m))
+# k is a float
+
